df762d202396490da838406e2c627bfe4f238b73
bparento <sp3643@drexel.edu>
2023-10-24 13:16:53 -0400

    Updated test_ticTacToe and ticTacToe with comments

--- src/homework_2_pyscaffold_bparento/ticTacToe.py
+++ src/homework_2_pyscaffold_bparento/ticTacToe.py
@@ -1,18 +1,25 @@
 class TicTacToe:
     def __init__(self):
+        # Initialize the game board with empty spaces and set the current player to "X"
         self.board = [" " for _ in range(9)]
         self.current_player = "X"
 
     def make_move(self, position):
+        # Method for making a move in the game
+        # Check if the specified position is valid (0-8) and if it's an empty space
         if 0 <= position < 9 and self.board[position] == " ":
+            # Place the current player's symbol in the specified position
             self.board[position] = self.current_player
-            return True
-        return False
+            return True  # Return True to indicate a successful move
+        return False  # Return False to indicate an invalid move
 
     def switch_player(self):
+        # Method for switching the current player from "X" to "O" or vice versa
         self.current_player = "X" if self.current_player == "O" else "O"
 
     def check_winner(self):
+        # Method for checking if there's a winner or a draw
+        # Define the winning conditions for rows, columns, and diagonals
         win_conditions = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Rows
             [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Columns
@@ -21,10 +28,14 @@ class TicTacToe:
 
         for condition in win_conditions:
             a, b, c = condition
+            # Check if the symbols in the winning condition are the same and not empty
             if self.board[a] == self.board[b] == self.board[c] != " ":
-                return self.board[a]
+                return self.board[a]  # Return the symbol of the winner
 
+        # If no winner has been found and the board is full, return "Draw"
         if " " not in self.board:
             return "Draw"
 
-        return None
+        return None  # If there's no winner or draw, return None
+
+# This is the end of the TicTacToe class definition.
--- tests/test_ticTacToe.py
+++ tests/test_ticTacToe.py
@@ -1,41 +1,70 @@
+# Import the unittest module for writing unit tests
 import unittest
+# Import the TicTacToe class from the specified module
 from homework_2_pyscaffold_bparento.ticTacToe import TicTacToe
 
+# Create a test case class that inherits from unittest.TestCase
 class TestTicTacToe(unittest.TestCase):
+    
+    # Method to set up the game before each test
     def setUp(self):
         self.game = TicTacToe()
 
+    # Test for the make_move method
     def test_make_move(self):
+        # Check if the game correctly allows the first move (0)
         self.assertTrue(self.game.make_move(0))
+        # Check if the game correctly disallows a move to an occupied position (0)
         self.assertFalse(self.game.make_move(0))
+        # Check if the game correctly allows the next move (1)
         self.assertTrue(self.game.make_move(1))
 
+    # Test for the switch_player method
     def test_switch_player(self):
+        # Check if the current player is initially "X"
         self.assertEqual(self.game.current_player, "X")
+        # Switch the player and verify if it's now "O"
         self.game.switch_player()
         self.assertEqual(self.game.current_player, "O")
+        # Switch the player again and verify if it's back to "X"
         self.game.switch_player()
         self.assertEqual(self.game.current_player, "X")
 
+    # Test for checking a draw scenario
     def test_check_winner_draw(self):
+        # Set up the game board with a draw scenario
         self.game.board = ["X", "O", "X", "X", "X", "O", "O", "X", "O"]
+        # Check if the game correctly identifies a draw
         self.assertEqual(self.game.check_winner(), "Draw")
 
+    # Test for checking a horizontal win scenario
     def test_check_winner_horizontal(self):
+        # Set up the game board with a horizontal win for "X"
         self.game.board = ["X", "X", "X", "O", "O", " ", "O", "X", "X"]
+        # Check if the game correctly identifies "X" as the winner
         self.assertEqual(self.game.check_winner(), "X")
 
+    # Test for checking a vertical win scenario
     def test_check_winner_vertical(self):
+        # Set up the game board with a vertical win for "X"
         self.game.board = ["X", "O", "X", "X", "O", "X", "O", "X", "O"]
+        # Check if the game correctly identifies "X" as the winner
         self.assertEqual(self.game.check_winner(), "X")
 
+    # Test for checking a diagonal win scenario
     def test_check_winner_diagonal(self):
+        # Set up the game board with a diagonal win for "X"
         self.game.board = ["X", "O", "O", "O", "X", "O", "O", "O", "X"]
+        # Check if the game correctly identifies "X" as the winner
         self.assertEqual(self.game.check_winner(), "X")
 
+    # Test for checking a scenario with no winner
     def test_check_winner_no_winner(self):
+        # Set up the game board with no clear winner
         self.game.board = ["X", "O", "X", "O", "X", "O", "O", "X", "O"]
+        # Check if the game correctly identifies that there is no winner
         self.assertIsNone(self.game.check_winner())
 
+# Entry point for running the unit tests when the script is executed
 if __name__ == "__main__":
     unittest.main()
