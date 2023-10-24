ff101b800754e80aaa7f89a226cab4664784972a
bparento <sp3643@drexel.edu>
2023-10-24 13:44:26 -0400

    Experiement using AI playing TicTacToe

--- src/homework_2_pyscaffold_bparento/ticTacToe.py
+++ src/homework_2_pyscaffold_bparento/ticTacToe.py
@@ -1,25 +1,78 @@
+# class TicTacToe:
+#     def __init__(self):
+#         self.board = [" " for _ in range(9)]
+#         self.current_player = "X"
+
+#     def make_move(self, position):
+#         if 0 <= position < 9 and self.board[position] == " ":
+#             self.board[position] = self.current_player
+#             return True
+#         return False
+
+#     def switch_player(self):
+#         self.current_player = "X" if self.current_player == "O" else "O"
+
+#     def check_winner(self):
+#         win_conditions = [
+#             [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Rows
+#             [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Columns
+#             [0, 4, 8], [2, 4, 6]  # Diagonals
+#         ]
+
+#         for condition in win_conditions:
+#             a, b, c = condition
+#             if self.board[a] == self.board[b] == self.board[c] != " ":
+#                 return self.board[a]
+
+#         if " " not in self.board:
+#             return "Draw"
+
+#         return None
+
+#     def display_board(self):
+#         # Function to display the current state of the game board
+#         print(f"{self.board[0]} | {self.board[1]} | {self.board[2]}")
+#         print("---------")
+#         print(f"{self.board[3]} | {self.board[4]} | {self.board[5]}")
+#         print("---------")
+#         print(f"{self.board[6]} | {self.board[7]} | {self.board[8]}")
+
+# def play_game():
+#     game = TicTacToe()
+#     while True:
+#         game.display_board()
+#         position = int(input(f"Player {game.current_player}, enter your move (0-8): "))
+#         if game.make_move(position):
+#             winner = game.check_winner()
+#             if winner:
+#                 game.display_board()
+#                 if winner == "Draw":
+#                     print("It's a draw!")
+#                 else:
+#                     print(f"Player {winner} wins!")
+#                 break
+#             game.switch_player()
+
+# if __name__ == "__main__":
+#     play_game()
+
+import random
+
 class TicTacToe:
     def __init__(self):
-        # Initialize the game board with empty spaces and set the current player to "X"
         self.board = [" " for _ in range(9)]
-        self.current_player = "X"
+        self.current_player = random.choice(["X", "O"])  # Randomly choose who goes first
 
     def make_move(self, position):
-        # Method for making a move in the game
-        # Check if the specified position is valid (0-8) and if it's an empty space
         if 0 <= position < 9 and self.board[position] == " ":
-            # Place the current player's symbol in the specified position
             self.board[position] = self.current_player
-            return True  # Return True to indicate a successful move
-        return False  # Return False to indicate an invalid move
+            return True
+        return False
 
     def switch_player(self):
-        # Method for switching the current player from "X" to "O" or vice versa
         self.current_player = "X" if self.current_player == "O" else "O"
 
     def check_winner(self):
-        # Method for checking if there's a winner or a draw
-        # Define the winning conditions for rows, columns, and diagonals
         win_conditions = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Rows
             [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Columns
@@ -28,14 +81,63 @@ class TicTacToe:
 
         for condition in win_conditions:
             a, b, c = condition
-            # Check if the symbols in the winning condition are the same and not empty
             if self.board[a] == self.board[b] == self.board[c] != " ":
-                return self.board[a]  # Return the symbol of the winner
+                return self.board[a]
 
-        # If no winner has been found and the board is full, return "Draw"
         if " " not in self.board:
             return "Draw"
 
-        return None  # If there's no winner or draw, return None
+        return None
 
-# This is the end of the TicTacToe class definition.
+    def display_board(self):
+        print(f"{self.board[0]} | {self.board[1]} | {self.board[2]}")
+        print("---------")
+        print(f"{self.board[3]} | {self.board[4]} | {self.board[5]}")
+        print("---------")
+        print(f"{self.board[6]} | {self.board[7]} | {self.board[8]}")
+
+def ai_make_move(game):
+    # A simple AI strategy: Win if possible, block the opponent if they are about to win, or make a random move
+    for position in range(9):
+        if game.board[position] == " ":
+            game.board[position] = game.current_player
+            if game.check_winner() == game.current_player:
+                return
+            game.board[position] = " "
+
+    for position in range(9):
+        if game.board[position] == " ":
+            game.board[position] = "X" if game.current_player == "O" else "O"
+            if game.check_winner() == "X" or game.check_winner() == "O":
+                game.board[position] = game.current_player
+                return
+            game.board[position] = " "
+
+    while True:
+        position = random.randint(0, 8)
+        if game.make_move(position):
+            return
+
+def self_play_game():
+    game = TicTacToe()
+    while True:
+        game.display_board()
+        if game.current_player == "X":
+            ai_make_move(game)
+        else:
+            while True:
+                position = random.randint(0, 8)
+                if game.make_move(position):
+                    break
+
+        winner = game.check_winner()
+        if winner:
+            game.display_board()
+            if winner == "Draw":
+                print("It's a draw!")
+            else:
+                print(f"Player {winner} wins!")
+            break
+
+if __name__ == "__main__":
+    self_play_game()
--- tests/test_ticTacToe.py
+++ tests/test_ticTacToe.py
@@ -1,70 +1,50 @@
 # Import the unittest module for writing unit tests
 import unittest
 # Import the TicTacToe class from the specified module
-from homework_2_pyscaffold_bparento.ticTacToe import TicTacToe
+from homework_2_pyscaffold_bparento.ticTacToe import TicTacToe, ai_make_move
 
-# Create a test case class that inherits from unittest.TestCase
-class TestTicTacToe(unittest.TestCase):
-    
-    # Method to set up the game before each test
-    def setUp(self):
-        self.game = TicTacToe()
+class TestTicTacToeAI(unittest.TestCase):
+    def test_make_move_valid(self):
+        game = TicTacToe()
+        self.assertTrue(game.make_move(0))
 
-    # Test for the make_move method
-    def test_make_move(self):
-        # Check if the game correctly allows the first move (0)
-        self.assertTrue(self.game.make_move(0))
-        # Check if the game correctly disallows a move to an occupied position (0)
-        self.assertFalse(self.game.make_move(0))
-        # Check if the game correctly allows the next move (1)
-        self.assertTrue(self.game.make_move(1))
+    def test_make_move_invalid(self):
+        game = TicTacToe()
+        game.make_move(0)
+        self.assertFalse(game.make_move(0))
 
-    # Test for the switch_player method
     def test_switch_player(self):
-        # Check if the current player is initially "X"
-        self.assertEqual(self.game.current_player, "X")
-        # Switch the player and verify if it's now "O"
-        self.game.switch_player()
-        self.assertEqual(self.game.current_player, "O")
-        # Switch the player again and verify if it's back to "X"
-        self.game.switch_player()
-        self.assertEqual(self.game.current_player, "X")
+        game = TicTacToe()
+        game.switch_player()
+        self.assertEqual(game.current_player, "O")
+        game.switch_player()
+        self.assertEqual(game.current_player, "X")
 
-    # Test for checking a draw scenario
     def test_check_winner_draw(self):
-        # Set up the game board with a draw scenario
-        self.game.board = ["X", "O", "X", "X", "X", "O", "O", "X", "O"]
-        # Check if the game correctly identifies a draw
-        self.assertEqual(self.game.check_winner(), "Draw")
+        game = TicTacToe()
+        game.board = ["X", "O", "X", "X", "O", "X", "O", "X", "O"]
+        self.assertEqual(game.check_winner(), "Draw")
 
-    # Test for checking a horizontal win scenario
     def test_check_winner_horizontal(self):
-        # Set up the game board with a horizontal win for "X"
-        self.game.board = ["X", "X", "X", "O", "O", " ", "O", "X", "X"]
-        # Check if the game correctly identifies "X" as the winner
-        self.assertEqual(self.game.check_winner(), "X")
+        game = TicTacToe()
+        game.board = ["X", "X", "X", "O", "O", " ", "O", "X", "X"]
+        self.assertEqual(game.check_winner(), "X")
 
-    # Test for checking a vertical win scenario
-    def test_check_winner_vertical(self):
-        # Set up the game board with a vertical win for "X"
-        self.game.board = ["X", "O", "X", "X", "O", "X", "O", "X", "O"]
-        # Check if the game correctly identifies "X" as the winner
-        self.assertEqual(self.game.check_winner(), "X")
+    def test_ai_make_move(self):
+        game = TicTacToe()
+        ai_make_move(game)  # Ensure the AI can make a move without errors
+        self.assertIsNotNone(game.check_winner())
+    
+    def test_switch_player_start_with_X(self):
+        game = TicTacToe()  # Create a new game, which starts with "X"
+        self.assertEqual(game.current_player, "X")
 
-    # Test for checking a diagonal win scenario
-    def test_check_winner_diagonal(self):
-        # Set up the game board with a diagonal win for "X"
-        self.game.board = ["X", "O", "O", "O", "X", "O", "O", "O", "X"]
-        # Check if the game correctly identifies "X" as the winner
-        self.assertEqual(self.game.check_winner(), "X")
+    def test_switch_player_after_switching(self):
+        game = TicTacToe()
+        initial_player = game.current_player
+        game.switch_player()  # Switch to the other player
+        self.assertNotEqual(initial_player, game.current_player)
+        self.assertEqual(game.current_player, "O") if initial_player == "X" else "X"
 
-    # Test for checking a scenario with no winner
-    def test_check_winner_no_winner(self):
-        # Set up the game board with no clear winner
-        self.game.board = ["X", "O", "X", "O", "X", "O", "O", "X", "O"]
-        # Check if the game correctly identifies that there is no winner
-        self.assertIsNone(self.game.check_winner())
-
-# Entry point for running the unit tests when the script is executed
 if __name__ == "__main__":
     unittest.main()
